a8b275f6e5a64e6b98c772df617e2dc2ba78cf28
dexter <178529795@qq.com>
2017-03-31 17:46:26 +0800

    更新

--- webapi/index.js
+++ webapi/index.js
@@ -13,38 +13,44 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/System/GetConfig', async(req, res) => {
-    let { version } = req.params
+    let { version } = req.query
     if (!version) res.status(200).send({ Status: 40002, Explain: "没有提供版本号" })
-    let config = await sequelize.query("select * from wf_system_appconfig where AppVersion = '" + version + "'")
-    config = config[0]
-    let setting = {}
-    for (let c of config) {
-        setting[c.AppKey] = c.AppVersion
+    try {
+        let config = await sequelize.query("select * from wf_system_appconfig where AppVersion = '" + version + "'")
+        config = config[0]
+        let setting = {}
+        for (let c of config) {
+            setting[c.AppKey] = c.AppVersion
+        }
+        res.status(200).send({ Status: 0, Explain: "", Config: setting })
+    } catch (ex) {
+        res.status(200).send({ Status: 500, Explain: ex })
     }
-    res.status(200).send({ Status: 0, Explain: "", Config: setting })
-        //     wf_system_appconfig
-        //       `Id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'app配置表',
-        //   `AppKey` varchar(128) COMMENT 'Key值',
-        //   `AppValue` varchar(128) COMMENT 'Value值',
-        //   `AppVersion` varchar(20) COMMENT 'App版本',
-        //   `CreateTime` datetime COMMENT '创建时间',
 })
 app.get('/v2.5/Choiceness/ChoicenessBannerList', async(req, res) => {
-    let result = await redisClient.getAsync("cacheResult:bannerChoice")
-    return res.status(200).send('{"Status":"0","Explain":"ok","Data":' + result + '}')
-        // let page = req.param("page", 0)
-        // let size = req.param("size", 10)
-        // console.log(page, size)
-        // return res.json({ page, size })
+    try {
+        let result = await redisClient.getAsync("cacheResult:bannerChoice")
+        return res.status(200).send('{"Status":"0","Explain":"ok","Data":' + result + '}')
+    } catch (ex) {
+        res.status(200).send({ Status: 500, Explain: ex })
+    }
+    // let page = req.param("page", 0)
+    // let size = req.param("size", 10)
+    // console.log(page, size)
+    // return res.json({ page, size })
 })
 app.get('/v2.5/Choiceness/ChoicenessList', async(req, res) => {
-    let result = await redisClient.getAsync("cacheResult:normalChoice")
-    return res.status(200).send('{"Status":"0","Explain":"ok","Data":' + result + '}')
-        // let page = req.param("page", 0)
-        // let size = req.param("size", 10)
-        // let maxId = req.param("maxId")
-        // console.log(page, size)
-        // return res.json({ page, size, maxId })
+    try {
+        let result = await redisClient.getAsync("cacheResult:normalChoice")
+        return res.status(200).send('{"Status":"0","Explain":"ok","Data":' + result + '}')
+    } catch (ex) {
+        res.status(200).send({ Status: 500, Explain: ex })
+    }
+    // let page = req.param("page", 0)
+    // let size = req.param("size", 10)
+    // let maxId = req.param("maxId")
+    // console.log(page, size)
+    // return res.json({ page, size, maxId })
 })
 app.get('/orders', [checkLogin(true)], async(req, res) => {
 
