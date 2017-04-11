51ab6c62210d6605e306e8e8e8952bcc349e5fb6
dexter <178529795@qq.com>
2017-04-11 20:41:21 +0800

    config

--- webapi/config.js
+++ webapi/config.js
@@ -8,11 +8,11 @@ let config = {
     clientInit: { //特定版本客户端配置
         "2.5": { showTrade: false }
     },
-    FinancialIndexBaseURL: "",
+    FinancialIndexBaseURL: "http://test.wolfstreet.tv/kmap/mark.html",
     FinancialIndex: {
-        hs: [{ Title: "上证指数", URL: "" }, { Title: "深证指数", URL: "" }, { Title: "创业指数", URL: "" }],
-        hk: [{ Title: "恒生指数", URL: "" }, { Title: "国企指数", URL: "" }, { Title: "红筹指数", URL: "" }],
-        us: [{ Title: "道琼斯", URL: "" }, { Title: "纳斯达克", URL: "" }, { Title: "标普500", URL: "" }],
+        hs: [{ Title: "上证指数", URL: "?code=0000001&type=hs" }, { Title: "深证指数", URL: "?code=1399001&type=hs" }, { Title: "创业指数", URL: "?code=1399006&type=hs" }],
+        hk: [{ Title: "恒生指数", URL: "?code=HSI&type=hk" }, { Title: "国企指数", URL: "?code=HSCEI&type=hk" }, { Title: "红筹指数", URL: "?code=HSCCI&type=hk" }],
+        us: [{ Title: "道琼斯", URL: "?code=DOWJONES&type=us" }, { Title: "纳斯达克", URL: "?code=NASDAQ&type=us" }, { Title: "标普500", URL: "?code=SP500&type=us" }],
         get sz() {
             return this.hs
         },
@@ -29,5 +29,8 @@ if (process.env.NODE_ENV === "production") {
 for (let k in config.clientInit) {
     config.clientInit[k] = Object.assign(Object.assign(Object.assign({}, config.clientInitDefault), config.clientInit[k]), config.clientInitAll)
 }
-//config.FinancialIndex
+(() => {
+    let { hs, hk, us } = config.FinancialIndex;
+    [].concat(hs, hk, us).forEach(x => x.URL = config.FinancialIndexBaseURL + x.URL)
+})()
 export default config
