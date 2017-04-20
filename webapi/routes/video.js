31ac8236ca2fa849c04790d6a9ccc056e0128910
dexter <178529795@qq.com>
2017-04-20 09:37:43 +0800

    sql 语法

--- webapi/routes/video.js
+++ webapi/routes/video.js
@@ -3,7 +3,7 @@ SELECT
 	room.RoomCode,room.RoomTitle,room.MemberCode,room.SecretType,room.City,room.CreateTime, c.Topic,(case when room.Status=0 then 'video' else 'live' end) Type,DATE_FORMAT(room.CreateTime,'%Y-%m-%d %H:%i:%s') CreateTime,
 	wf_member.HeadImage,wf_member.NickName,concat(:picBaseURL, ImageUrl) ImageUrl
 FROM
-	(Select * from wf_liveroom where Status < 2 order by RoomId Desc LIMIT :page*20,20) room
+	(Select * from wf_liveroom where Status < 2 order by RoomId Desc LIMIT :page,20) room
 LEFT JOIN (
 	SELECT
 		a.LiveRoomCode,
@@ -22,7 +22,7 @@ module.exports = function({ sequelize, statistic, ctt, express, config, wrap, re
     const router = express.Router();
     /**直播列表*/
     router.get('/LiveList/:page', wrap(async(req, res) => {
-        let replacements = { page: Number(req.params.page), picBaseURL: config.picBaseURL }
+        let replacements = { page: Number(req.params.page) * 20, picBaseURL: config.picBaseURL }
         let [result] = await sequelize.query(sql1, { replacements })
         let roomCodes = result.map(i => i.RoomCode)
         roomCodes = await redisClient.mgetAsync(...roomCodes.map(i => "roommemberrealcount:" + i.RoomCode).concat(roomCodes.map(i => "roomrobotcount:" + i.RoomCode)))
@@ -51,7 +51,7 @@ module.exports = function({ sequelize, statistic, ctt, express, config, wrap, re
     }));
     /**教学列表 */
     router.get('/Teaching/:page', wrap(async(req, res) => {
-        let [result] = await sequelize.query("select VideoCode Code,VideoName Title,concat(:picBaseURL, VideoImage)  Image,TimeLong ,DATE_FORMAT(ShowTime,'%Y-%m-%d %H:%i:%s') ShowTime from wf_live_video where Backup1=3 and Status=0 limit :page*20,20", { replacements: { page: Number(req.params.page), picBaseURL: config.picBaseURL } })
+        let [result] = await sequelize.query("select VideoCode Code,VideoName Title,concat(:picBaseURL, VideoImage)  Image,TimeLong ,DATE_FORMAT(ShowTime,'%Y-%m-%d %H:%i:%s') ShowTime from wf_live_video where Backup1=3 and Status=0 limit :page,20", { replacements: { page: Number(req.params.page) * 20, picBaseURL: config.picBaseURL } })
         res.send({ Status: 0, Explain: "", DataList: result })
     }));
     /**热门分类 */
@@ -70,12 +70,12 @@ module.exports = function({ sequelize, statistic, ctt, express, config, wrap, re
     // }));
     /**某专题视频列表*/
     router.get('/Dissertation/:Id/:page', wrap(async(req, res) => {
-        let [Dissertation] = await sequelize.query("select VideoCode Code,VideoName Title,concat(:picBaseURL, VideoImage)  Image,TimeLong,DATE_FORMAT(ShowTime,'%Y-%m-%d %H:%i:%s') ShowTime from wf_live_video where Status=0 and DissertationId=:Id order by ShowTime desc limit :page*20,20", { replacements: { page: Number(req.params.page), picBaseURL: config.picBaseURL, Id: Number(req.params.Id) } })
+        let [Dissertation] = await sequelize.query("select VideoCode Code,VideoName Title,concat(:picBaseURL, VideoImage)  Image,TimeLong,DATE_FORMAT(ShowTime,'%Y-%m-%d %H:%i:%s') ShowTime from wf_live_video where Status=0 and DissertationId=:Id order by ShowTime desc limit :page,20", { replacements: { page: Number(req.params.page) * 20, picBaseURL: config.picBaseURL, Id: Number(req.params.Id) } })
         res.send({ Status: 0, Explain: "", DataList: Dissertation })
     }));
     /**最新视频列表 */
     router.get('/Lastest/:page', wrap(async(req, res) => {
-        let [result] = await sequelize.query("select VideoCode Code,VideoName Title,concat(:picBaseURL, VideoImage) Image,TimeLong,DATE_FORMAT(ShowTime,'%Y-%m-%d %H:%i:%s') ShowTime from wf_live_video where Status=0 order by ShowTime desc limit :page*20,20", { replacements: { page: Number(req.params.page), picBaseURL: config.picBaseURL } })
+        let [result] = await sequelize.query("select VideoCode Code,VideoName Title,concat(:picBaseURL, VideoImage) Image,TimeLong,DATE_FORMAT(ShowTime,'%Y-%m-%d %H:%i:%s') ShowTime from wf_live_video where Status=0 order by ShowTime desc limit :page,20", { replacements: { page: Number(req.params.page) * 20, picBaseURL: config.picBaseURL } })
         res.send({ Status: 0, Explain: "", DataList: result })
     }));
     return router;
