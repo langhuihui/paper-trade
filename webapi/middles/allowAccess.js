f4e48ab9e07722ac90195501f8c3c08d2437753c
pg <pg830616@163.com>
2017-05-03 11:45:45 +0800

    跨域问题修改

--- webapi/middles/allowAccess.js
+++ webapi/middles/allowAccess.js
@@ -4,6 +4,7 @@ import Config from '../../config'
 function allowAccess(method = "GET") {
     return function(req, res, next) {
         res.set("Access-Control-Allow-Origin", Config.ajaxOrigin);
+        //res.set("Access-Control-Allow-Headers", "X-Requested-With");
         res.set("Access-Control-Allow-Methods", method);
         next()
     }
