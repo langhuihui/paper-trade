5fb9ca82b3676ed3caad602cb983a35b331f1102
pg <pg830616@163.com>
2017-05-10 18:54:59 +0800

    收益计算修改

--- common/everyDays/totalAssets.js
+++ common/everyDays/totalAssets.js
@@ -12,75 +12,79 @@ export default new EveryDay('totalAssets', "05:00:00", async() => {
     switch (moment().day()) {
         case 1:
         case 0: //周日和周一取上周二的数据
-            LastDate = moment().day(-5).format();
+            LastDate = moment().day(-5).format('YYYY-MM-DD');
             break;
         case 2: //周二到周六取本周二的数据
         case 3:
         case 4:
         case 5:
         case 6:
-            LastDate = moment().day(2).format();
+            LastDate = moment().day(2).format('YYYY-MM-DD');
             break;
     }
-    let [result] = await mainDB.query('select * from wf_drivewealth_practice_account where MemberCode not in (select MemberCode from wf_stockcompetitionmember where Source=1)')
-    for (let { UserId, MemberCode, username, password, emailAddress1 }
-        of result) {
-        try {
-            let [todayAssetResult] = await mainDB.query("select * from wf_drivewealth_practice_asset where EndDate=CurDate() and UserId=:UserId", { replacements: { UserId } })
-            if (todayAssetResult.length) continue
-            let { sessionKey, accounts } = await request({
-                uri: dwUrls.createSession,
-                method: "POST",
-                body: {
-                    "appTypeID": "2000",
-                    "appVersion": "0.1",
-                    username,
-                    "emailAddress": emailAddress1,
-                    "ipAddress": "1.1.1.1",
-                    "languageID": "zh_CN",
-                    "osVersion": "iOS 9.1",
-                    "osType": "iOS",
-                    "scrRes": "1920x1080",
-                    password
-                },
-                json: true
-            })
-            let [{ cash, accountNo, accountID }] = accounts
-            let { positions } = await request({
-                headers: { 'x-mysolomeo-session-key': sessionKey },
-                qs: {
-                    sessionKey,
-                    ReportName: "PositionRestingOrder",
-                    ReportFormat: "JSON",
-                    AccountNumber: accountNo
-                },
-                uri: dwUrls.position,
-                method: "POST",
-                json: true
-            })
-            if (positions) {
+    while (true) {
+        let [result] = await mainDB.query('select wf_drivewealth_practice_account.* from wf_drivewealth_practice_account where MemberCode not in (select MemberCode from wf_stockcompetitionmember where Source=1)and `MemberCode`  not in (SELECT `MemberCode` from `wf_drivewealth_practice_asset` WHERE EndDate = CurDate())')
+        if (!result.length)
+            break
+        for (let { UserId, MemberCode, username, password, emailAddress1 }
+            of result) {
+            try {
+                if (todayAssetResult.length) continue
+                let { sessionKey, accounts } = await request({
+                    uri: dwUrls.createSession,
+                    method: "POST",
+                    body: {
+                        "appTypeID": "2000",
+                        "appVersion": "0.1",
+                        username,
+                        "emailAddress": emailAddress1,
+                        "ipAddress": "1.1.1.1",
+                        "languageID": "zh_CN",
+                        "osVersion": "iOS 9.1",
+                        "osType": "iOS",
+                        "scrRes": "1920x1080",
+                        password
+                    },
+                    json: true
+                })
+                let [{ cash, accountNo, accountID }] = accounts
+                let { positions } = await request({
+                    headers: { 'x-mysolomeo-session-key': sessionKey },
+                    qs: {
+                        sessionKey,
+                        ReportName: "PositionRestingOrder",
+                        ReportFormat: "JSON",
+                        AccountNumber: accountNo
+                    },
+                    uri: dwUrls.position,
+                    method: "POST",
+                    json: true
+                })
+                if (positions) {
 
-                let Positions = positions.reduce((acc, val) => acc + val.mtm, 0) //总的持仓资产
-                let MtmPL = positions.reduce((acc, val) => acc + val.mtmPL, 0) //总的持仓浮动盈亏
-                let replacements = { UserId, MemberCode, AccountID: accountID, Balance: cash, Positions, TotalAmount: cash + Positions, MtmPL }
-                let [result] = await mainDB.query('select TotalAmount from wf_drivewealth_practice_asset where UserId=:UserId and EndDate<CurDate() order by EndDate desc limit 1', { replacements })
+                    let Positions = positions.reduce((acc, val) => acc + val.mtm, 0) //总的持仓资产
+                    let MtmPL = positions.reduce((acc, val) => acc + val.mtmPL, 0) //总的持仓浮动盈亏
+                    let replacements = { UserId, MemberCode, AccountID: accountID, Balance: cash, Positions, TotalAmount: cash + Positions, MtmPL }
+                    let [result] = await mainDB.query('select TotalAmount from wf_drivewealth_practice_asset where UserId=:UserId and EndDate<CurDate() order by EndDate desc limit 1', { replacements })
 
-                let [weekresult] = await mainDB.query('select TotalAmount from wf_drivewealth_practice_asset where UserId=:UserId and EndDate<:LastDate order by EndDate desc limit 1', { replacements: { LastDate, UserId } })
-                replacements.TodayProfit = replacements.TotalAmount - (result.length ? result[0].TotalAmount : Config.practiceInitFun)
-                replacements.WeekProfit = replacements.TotalAmount - (weekresult.length ? weekresult[0].TotalAmount : Config.practiceInitFun)
-                replacements.WeekYield = replacements.WeekProfit / replacements.TotalAmount * 100
-                replacements.TotalProfit = replacements.TotalAmount - Config.practiceInitFun
-                replacements.TotalYield = replacements.TotalProfit / Config.practiceInitFun * 100
-                await mainDB.query(sqlstr.insert("wf_drivewealth_practice_asset", replacements, { CreateTime: "now()", EndDate: "curDate()" }), { replacements })
-            } else {
-                let replacements = { UserId, MemberCode, AccountID: accountID, Balance: cash, Positions: 0, TotalAmount: cash, MtmPL: 0, TodayProfit: 0 }
-                await mainDB.query(sqlstr.insert("wf_drivewealth_practice_asset", replacements, { CreateTime: "now()", EndDate: "curDate()" }), { replacements })
+                    let [weekresult] = await mainDB.query('select TotalAmount from wf_drivewealth_practice_asset where UserId=:UserId and EndDate<:LastDate order by EndDate desc limit 1', { replacements: { LastDate, UserId } })
+                    replacements.TodayProfit = replacements.TotalAmount - (result.length ? result[0].TotalAmount : Config.practiceInitFun)
+                    replacements.WeekProfit = replacements.TotalAmount - (weekresult.length ? weekresult[0].TotalAmount : Config.practiceInitFun)
+                    replacements.WeekYield = replacements.WeekProfit / replacements.TotalAmount * 100
+                    replacements.TotalProfit = replacements.TotalAmount - Config.practiceInitFun
+                    replacements.TotalYield = replacements.TotalProfit / Config.practiceInitFun * 100
+                    await mainDB.query(sqlstr.insert("wf_drivewealth_practice_asset", replacements, { CreateTime: "now()", EndDate: "curDate()" }), { replacements })
+                } else {
+                    let replacements = { UserId, MemberCode, AccountID: accountID, Balance: cash, Positions: 0, TotalAmount: cash, MtmPL: 0, TodayProfit: 0 }
+                    await mainDB.query(sqlstr.insert("wf_drivewealth_practice_asset", replacements, { CreateTime: "now()", EndDate: "curDate()" }), { replacements })
+                }
+            } catch (ex) {
+                console.error(new Date(), ex)
+                continue;
             }
-        } catch (ex) {
-            console.error(new Date(), ex)
-            continue;
         }
     }
+
 
     //活动期间将真数据插入wf_drivewealth_practice_asset_v
     await mainDB.query("insert into wf_drivewealth_practice_asset_v(UserId,AccountID,Balance,MtmPL,Positions,TodayProfit,TodayYield,WeekProfit,WeekYield,MonthProfit,MonthYield,YearProfit,YearYield,TotalProfit,TotalYield,TotalAmount,MemberCode,EndDate,CreateTime) select UserId,AccountID,Balance,MtmPL,Positions,TodayProfit,TodayYield,WeekProfit,WeekYield,MonthProfit,MonthYield,YearProfit,YearYield,TotalProfit,TotalYield,TotalAmount,MemberCode,EndDate,CreateTime from wf_drivewealth_practice_asset where EndDate=CurDate() and MemberCode in(select MemberCode from wf_stockcompetitionmember where Source<>1)");
