8b73234c7e38c06d7e06a4ab1b540ccdcb742c9a
pg <pg830616@163.com>
2017-05-26 21:40:41 +0800

    改进获取嘉维实时数据

--- getDWData/index.js
+++ getDWData/index.js
@@ -196,17 +196,17 @@ async function getSessionKey() {
                 body: {
                     "appTypeID": "2000",
                     "appVersion": "0.1",
-                    "username": "36300888",
-                    //"username": "16459847",
-                    "emailAddress": "36300888@wolfstreet.tv",
-                    //"emailAddress": "16459847@wolfstreet.tv",
+                    //"username": "36300888",
+                    "username": "16459847",
+                    //"emailAddress": "36300888@wolfstreet.tv",
+                    "emailAddress": "16459847@wolfstreet.tv",
                     "ipAddress": "1.1.1.1",
                     "languageID": "zh_CN",
                     "osVersion": "iOS 9.1",
                     "osType": "iOS",
                     "scrRes": "1920x1080",
-                    "password": "p36300888"
-                        //"password": "p16459847"
+                    //"password": "p36300888"
+                    "password": "p16459847"
                 },
                 json: true
             }))
