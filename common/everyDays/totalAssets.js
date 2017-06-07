f7f1c5e6542ea8376f424b5d6470702de2a13361
pg <pg830616@163.com>
2017-06-07 19:34:48 +0800

    关闭机器人增加收益

--- common/everyDays/totalAssets.js
+++ common/everyDays/totalAssets.js
@@ -154,14 +154,15 @@ export default new EveryDay('totalAssets', "04:30:00", async() => {
             let replacements = { UserId, MemberCode }
             let [fakeresult] = await mainDB.query('select TotalAmount from wf_drivewealth_practice_asset_v where UserId=:UserId and EndDate<CurDate() order by EndDate desc limit 1', { replacements })
             let [fakeweekresult] = await mainDB.query("select TotalAmount from wf_drivewealth_practice_asset_v where UserId=:UserId and EndDate<:LastDate order by EndDate desc limit 1 ", { replacements: { LastDate, UserId } });
-            if (moment().day() == 0 || moment().day() == 1 || moment().format('YYYY-MM-DD') == "2017-05-29") {
+            /*if (moment().day() == 0 || moment().day() == 1 || moment().format('YYYY-MM-DD') == "2017-05-29") {
                 replacements.TotalAmount = fakeresult.length ? fakeresult[0].TotalAmount : Config.practiceInitFun
             } else {
                 let rand = 1 + Math.random() * Config.randmax / 100
                 rand = rand.toFixed(4)
                 replacements.TotalAmount = fakeresult.length ? fakeresult[0].TotalAmount * rand : Config.practiceInitFun * rand
-            }
+            }*/
 
+            replacements.TotalAmount = fakeresult.length ? fakeresult[0].TotalAmount : Config.practiceInitFun
             replacements.TodayProfit = replacements.TotalAmount - (fakeresult.length ? fakeresult[0].TotalAmount : Config.practiceInitFun)
             replacements.WeekProfit = replacements.TotalAmount - (fakeweekresult.length ? fakeweekresult[0].TotalAmount : Config.practiceInitFun)
             replacements.WeekYield = replacements.WeekProfit / (fakeweekresult.length ? fakeweekresult[0].TotalAmount : Config.practiceInitFun) * 100
