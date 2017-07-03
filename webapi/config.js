270599c8ab310121af266f57280fbdf2afcd0b82
dexter <178529795@qq.com>
2017-07-03 10:48:57 +0800

    测试环境改成字符串

--- webapi/config.js
+++ webapi/config.js
@@ -1,7 +1,7 @@
 let config = {
     port: 10002,
     tokenTime: 14400000,
-    clientInitAll: { forceUpdate: 2.4, quotation: { source: "sina" }, dbVersion: 1, showRank: true }, //所有客户端配置
+    clientInitAll: { forceUpdate: "2.4", quotation: { source: "sina" }, dbVersion: 1, showRank: true }, //所有客户端配置
     clientInitDefault: {
         showTrade: true
     }, //默认配置
